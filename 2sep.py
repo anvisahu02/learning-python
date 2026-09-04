3864842f2bf5c414457d05d890c2a03c1c99d1be
Anvi <anvisahu3065@gmail.com>
2026-09-04 12:32:48 +0530

    Add files via upload

---
+++ 2sep.py
@@ -0,0 +1,52 @@
+#variable naming system (charatture, underscope, number)
+#never start from number
+# a10=20
+# a_1=30
+# _a1=20
+# 1_a= 10 # invalide
+
+#Q1 addition / sub/ div/ mod / power / multi of two number
+
+# a= int(input("enter 1 no.:"))
+# b= int(input("enter 2 no.:"))
+# #print(a+b) #output in float
+# print(int(a+b)) # output in int
+# we do typecasting to convert float output into int
+
+# kitne variable lege- q ke according formula pr depend krega
+
+#WAP to convert rupees into paisa
+r= int(input("Enter rupees: "))
+print(r/100)
+
+#WAP to gram into kg
+g= int(input("Enter gram:"))
+print(int(g/1000))
+
+#WAP to convert hours into milisecond
+h= int(input("Enter hour"))
+print(h*3600000)
+
+#wap to convert centimeter into meter
+cm=int(input("Enter Centimeter no.: "))
+print(cm/100)
+
+# WAP to convert km into meter
+km=int(input("Enter kilo Meter: "))
+print(km*1000)
+
+#wap to convert quintal into gram
+
+
+#wap to convert dollar into rupees
+d=int(input("Enter dollar value: "))
+print(d/100)
+
+#wap to convert second into hours
+s= int(input("Enter second value: "))
+print(int(s/3600))
+
+#WAP to convert kilogram into Gram
+kg=int(input("enter kg:"))
+print(kg*1000)
+
