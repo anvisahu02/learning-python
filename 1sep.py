87bc4dcdfe1869aad78d0f7e0ef85ea935fad8c7
Anvi <anvisahu3065@gmail.com>
2026-09-04 12:32:30 +0530

    Add files via upload

---
+++ 1sep.py
@@ -0,0 +1,25 @@
+#WAP for arithmetic operator
+
+# a= int(input("enter 1 no. "))
+# b= int(input("enter 2 no. "))
+# c=a+b
+# d=a-b
+# e=a*b
+# f=a/b
+# g=a**b
+# h=a%b
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+# print(g)
+# print(h)
+
+# WAP to display simple intrest
+
+p= int(input("Enter principal value:"))
+r= int(input("Enter rate value:")) # here we also use float # int is less priority then float
+t= int(input("Enter time value:"))
+si=(p*r*t)/100
+print(si)
+
